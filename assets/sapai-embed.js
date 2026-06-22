386237927c8ca7e2fc535e0a4dad71e9efe15e85
Adrian <adrianpurnama209@gmail.com>
2026-06-22 11:49:08 +0700

    fix sapai embed

--- assets/sapai-embed.js
+++ assets/sapai-embed.js
@@ -1,46 +1,47 @@
 /* SapAi embed widget */
 (function () {
-  if (window.__sapaiEmbedInit) return;
-  window.__sapaiEmbedInit = true;
+  var SAPAI_ORIGIN = "https://sapai.amfphub.com";
 
-  function init() {
+  function openChat() {
     var iframe = document.getElementById("sapai-embed-widget");
     var launcher = document.getElementById("sapai-embed-launcher");
     if (!iframe || !launcher) return;
 
-    var embedSrc = iframe.getAttribute("src") || "";
-    var wasClosedByUser = false;
+    var embedSrc = iframe.getAttribute("data-embed-src") || iframe.getAttribute("src") || "";
+    window.__sapaiIgnoreCloseUntil = Date.now() + 1500;
 
-    function showChat() {
-      if (wasClosedByUser && embedSrc) {
-        iframe.src = embedSrc;
-        wasClosedByUser = false;
-      }
-      iframe.classList.add("is-open");
-      launcher.classList.add("is-hidden");
+    launcher.classList.add("is-hidden");
+    iframe.classList.add("is-open");
+
+    if (embedSrc) {
+      iframe.src = embedSrc;
     }
-
-    function hideChat(markClosed) {
-      iframe.classList.remove("is-open");
-      launcher.classList.remove("is-hidden");
-      if (markClosed) wasClosedByUser = true;
-    }
-
-    launcher.addEventListener("click", showChat);
-
-    window.addEventListener("message", function (e) {
-      var d = e.data;
-      if (!d || d.type !== "sapai-embed") return;
-      if (d.action === "close") hideChat(true);
-      if (d.action === "open") showChat();
-    });
-
-    hideChat(false);
   }
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", init);
-  } else {
-    init();
+  function closeChat() {
+    if (window.__sapaiIgnoreCloseUntil && Date.now() < window.__sapaiIgnoreCloseUntil) {
+      return;
+    }
+
+    var iframe = document.getElementById("sapai-embed-widget");
+    var launcher = document.getElementById("sapai-embed-launcher");
+    if (!iframe || !launcher) return;
+
+    iframe.classList.remove("is-open");
+    launcher.classList.remove("is-hidden");
+  }
+
+  window.sapaiEmbedOpen = openChat;
+  window.sapaiEmbedClose = closeChat;
+
+  if (!window.__sapaiEmbedMessageBound) {
+    window.__sapaiEmbedMessageBound = true;
+    window.addEventListener("message", function (e) {
+      if (e.origin !== SAPAI_ORIGIN) return;
+      var d = e.data;
+      if (!d || d.type !== "sapai-embed") return;
+      if (d.action === "close") closeChat();
+      if (d.action === "open") openChat();
+    });
   }
 })();
